2a47c00fa0127cadadd6d284b13d6679d8fb72af
Andrei Dudau <andrei@takeofflabs.com>
2019-06-27 21:00:34 +0300

    remove extra code that didn't work

--- mas-vacuum/Models/Room.swift
+++ mas-vacuum/Models/Room.swift
@@ -9,62 +9,13 @@
 import Foundation
 
 class Room {
-    private let queue = DispatchQueue(label: Constants.queueName, attributes: .concurrent)
+    var agents: [Vacuum] = []
+    var agentPositions: [Position] = []
+    var floor: [[State]] = [
+                    [.clean, .clean, .dirt, .clean],
+                    [.clean, .dirt, .clean, .clean],
+                    [.clean, .dirt, .dirt, .clean],
+                    [.dirt, .clean, .clean, .clean]
+                            ]
     
-    private var _agents: [Vacuum] = []
-    var agents: [Vacuum] {
-        var result: [Vacuum] = []
-        queue.async {
-            result = self._agents
-        }
-        return result
-    }
-    
-    private var _agentPositions: [Position] = []
-    var agentPositions: [Position] {
-        var result: [Position] = []
-        queue.async {
-            result = self._agentPositions
-        }
-        return result
-    }
-    
-    private var _floor: [[State]] = [
-        [.clean, .clean, .dirt, .clean],
-        [.clean, .dirt, .clean, .clean],
-        [.clean, .dirt, .dirt, .clean],
-        [.dirt, .clean, .clean, .clean]
-    ]
-    var floor: [[State]] {
-        var result: [[State]] = []
-        queue.async {
-            result = self._floor
-        }
-        return result
-    }
-    
-    func updatePosition(vacuum: Vacuum, position: Position) {
-        queue.async(flags: .barrier) {
-            self._agentPositions[vacuum.id] = position
-        }
-    }
-    
-    func updateFloor(at position: Position, with state: State) {
-        queue.async(flags: .barrier) {
-            self._floor[position.x][position.y] = state
-        }
-    }
-}
-
-extension Room: CustomStringConvertible {
-    var description: String {
-        var string = "and"
-        for i in 0..<floor.count {
-            for j in 0..<floor[i].count {
-                string += String(floor[i][j].rawValue) + " "
-            }
-            string += "\n"
-        }
-        return string
-    }
 }
